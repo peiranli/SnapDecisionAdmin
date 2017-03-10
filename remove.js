e691c805a29137683a0b0ecbd5c4787c6235c450
Peiran Li <pel047@ucsd.edu>
2017-03-09 17:32:45 -0800

    Add remove root reference

--- remove.js
+++ remove.js
@@ -10,7 +10,11 @@ var db = admin.database();
 //get the root reference
 var dataRef = db.ref("data/");
 var userRef = db.ref("users/");
+var rootRef = db.ref();
 
+rootRef.remove(function(error){
+    console.log(error);
+});
 
 dataRef.remove(function(error){
     //do stuff after removal
